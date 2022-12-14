aeb89a4873a1aec2ba8801d8f2772f5bc95d896c
lverweijen <lverweijen>
2022-12-14 22:45:51 +0100

    Make tableresult more flexible

--- piargus/tableresult.py
+++ piargus/tableresult.py
@@ -24,30 +24,27 @@ class TableResult:
         return self._df[self._response]
 
     @property
-    def safe(self):
-        if 'safe' not in self._df.columns:
-            self._df['safe'] = self.unsafe
-            self._df.loc[~self.status_code.isin(['S', 'P', 'Z']), 'safe'] = 'x'
-        return self._df['safe']
-
-    @property
     def status_num(self):
         return self._df['Status']
 
-    @property
-    def status_code(self):
-        if 'status_code' not in self._df.columns:
-            self._df['status_code'] = '?'
-            for code, nums in STATUS_CODES.items():
-                self._df.loc[self.status_num.isin(nums), 'status_code'] = code
+    def make_safe(self, marker='x'):
+        safe = self.unsafe.copy()
+        status_code = self.get_status_code()
+        safe[~status_code.isin(['S', 'P', 'Z'])] = marker
+        return safe
 
-        return self._df['status_code']
+    def get_status_code(self):
+        status_code = pd.Series('?', index=self.status_num.index)
+        for code, nums in STATUS_CODES.items():
+            status_code[self.status_num.isin(nums)] = code
+
+        return status_code
 
     def __str__(self):
         return f"Response: {self.response}\n{self.dataframe()}"
 
     def dataframe(self):
         return pd.DataFrame({
-            'safe': self.safe,
-            'status_code': self.status_code,
+            'safe': self.make_safe(),
+            'status': self.get_status_code(),
             'unsafe': self.unsafe})
